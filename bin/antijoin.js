fed764554968fe3c0f6083c03c96e9dce7a4c4d5
Max Murphy-Skvorzov <mmurphy@telemetry.com>
2014-10-06 19:07:55 +0100

    += antijoin

---
+++ bin/antijoin.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+// Sorts a file where each line is a JSON object by the provided key:
+
+var by    = process.argv[2]
+  , files = process.argv.slice(3)
+  , fs    = require('fs')
+  , split = require('split');
+
+var ans = {};
+
+fs.createReadStream(files[0])
+    .pipe(split())
+    .on('data', function (line) {
+        var record;
+        try{
+            record = JSON.parse(line);
+        }catch(e){return;}
+        ans[record[by]] = record;
+    })
+    .on('end', function(){
+        var survivors = ans;
+        ans = {};
+        fs.createReadStream(files[1])
+        .pipe(split())
+        .on('data', function(line){
+            var record;
+            try {
+                record = JSON.parse(line);
+            } catch(e){return;}
+            var combined = survivors[record[by]];
+            if (combined === undefined){
+                console.log(line);
+            }
+        });
+    });
+
