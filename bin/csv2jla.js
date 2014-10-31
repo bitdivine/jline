c0404a64a38e4e9e04491c2729db93c1fa6fa6e1
Max Murphy-Skvorzov <mmurphy@telemetry.com>
2014-10-31 19:56:59 +0000

    += basic csv ingester.  No fancy options.

---
+++ bin/csv2jla.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+
+if(require.main === module) {
+    if (process.argv[2] === '--help') {
+        console.error(require('fs').readFileSync(__filename.replace(/.js$/,'.md'),{encoding:'utf8'}));
+        process.exit(2);
+    }
+
+    var csv = require('ya-csv');
+    var reader = csv.createCsvStreamReader(process.openStdin(), {
+        'separator': ',',
+        'quote': '"',
+        'escape': '"',       
+        'comment': '',
+    });
+    var writer = csv.createCsvStreamWriter(process.stdout);
+    var header = null;
+    reader.addListener('data', function(data) {
+        console.log(JSON.stringify(data));
+    });
+}
