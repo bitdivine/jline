6cc33729ae70025ff14f7cc1dfd21475f730eaa1
Max Murphy-Skvorzov <mmurphy@telemetry.com>
2014-11-03 17:41:40 +0000

    += compare

---
+++ bin/compare.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+// Sorts a file where each line is a JSON object by the provided key:
+
+var by    = process.argv[2]
+  , files = process.argv.slice(3)
+  , fs    = require('fs')
+  , split = require('split');
+
+var lefts = {};
+var ans = {};
+
+fs.createReadStream(files[0])
+    .pipe(split())
+    .on('data', function (line) {
+        var record;
+        try{
+            record = JSON.parse(line);
+        }catch(e){return;}
+        lefts[record[by]] = record;
+    })
+    .on('end', function(){
+        fs.createReadStream(files[1])
+        .pipe(split())
+        .on('data', function(line){
+            var record;
+            try {
+                record = JSON.parse(line);
+            } catch(e){return;}
+            var left = lefts[record[by]];
+            if (left !== undefined){
+                var combined = {};
+                Object.keys(record).forEach(function(k){
+                    if (undefined !== left[k]){
+                        combined[k] = [left[k],record[k]];
+                    }
+                });
+                ans[record[by]] = combined;
+            }
+        })
+        .on('end', function(){
+            Object.keys(ans).forEach(function(k){console.log(JSON.stringify(ans[k]));});
+        });
+    });
+
