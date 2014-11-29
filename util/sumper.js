c6f71c1f8447771468c1caaaa4410d504c4a3b81
Max Murphy-Skvorzov <mmurphy@telemetry.com>
2014-11-29 16:01:41 +0000

    += bugfix

--- util/sumper.js
+++ util/sumper.js
@@ -5,7 +5,7 @@
 # Usage:
 # cat your.jlines | jline-numper
 
-path="$(./bin/parsePath.js "$1" | jline-foreach 'console.log(record.map(function(a){return JSON.stringify([a]);}).join(""))')"
+path="$(jline-parsePath "$1" | jline-foreach 'console.log(record.map(function(a){return JSON.stringify([a]);}).join(""))')"
 echo "Collecting $path" >&2
 
 jline-foreach \
