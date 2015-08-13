6e9fe241e3ed5df598236c9f8e08c68c25bf1a68
Max Murphy-Skvorzov <mmurphy@telemetry.com>
2015-08-13 14:46:43 +0100

    Make the interval configrable using environment variables.

--- util/sumper.js
+++ util/sumper.js
@@ -9,8 +9,8 @@ path="$(jline-parsePath "$1" | jline-foreach 'console.log(record.map(function(a)
 echo "Collecting $path" >&2
 
 jline-foreach \
-'start::global.a = new(require("numper"))({interval:1000});a.on("aggregate", emit)' \
-"jline::var val=Number(record$path);if(!isNaN(val))a.increment('sum',val)"
+'start::global.a = new(require("numper"))({interval:process.env.interval||1000});a.on("aggregate", emit)' \
+"jline::var val=Number(record$path);if(!isNaN(val)){a.increment('sum',val); a.increment('count',1);}"
 
 
 
