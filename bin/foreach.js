516122b1e7e5ee6f64d918d1e3328c52050286e4
Max Murphy-Skvorzov <mmurphy@telemetry.com>
2015-08-13 14:44:51 +0100

    +=getpath and setpath

--- bin/foreach.js
+++ bin/foreach.js
@@ -30,12 +30,20 @@ function keyvals(dict, callback){
     Object.keys(dict).forEach(function(k){callback(k,dict[k]);});
 }
 
+function setpath(subject,path,value){
+	var c=path.length;
+	path.reduce(function(s,n){return s[n]=--c?(s[n]||{}):value;var after=s[n];},subject);
+}
+function getpath(subject,path){
+	return path.reduce(function(s,n){if(s!==undefined)return s[n];},subject);
+}
+
 var source = require('./clean')(process.stdin);
 source.on('comment', function(c){console.log(c);});
 eventHandlers.forEach(function(tuple){
     var event = tuple[0].toLowerCase();
-    var code  = Function('require','emit','keyvals', 'record', 'lineNumber', 'line', 'recordNumber', tuple[1])
-               .bind(null,req,      emit,  keyvals);
+    var code  = Function('require','emit','keyvals','setpath','getpath', 'record', 'lineNumber', 'line', 'recordNumber', tuple[1])
+               .bind(null,req,      emit,  keyvals , setpath , getpath);
     code.name = event;
     if (['beg','begin','start','beginning'].indexOf(event)!== -1) code();
     else source.on(event, code);
